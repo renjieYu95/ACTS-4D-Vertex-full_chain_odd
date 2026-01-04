7900d778170c6d24a789ae83e1e7c8c2766931f0
Renjie Yu <reyu@lxplus989.cern.ch>
2026-01-04 08:52:53 +0100

    plot some hypothesis

---
+++ fixparticleHypothesis/plot/gauss_slice.py
@@ -0,0 +1,67 @@
+#切片，单高斯
+import ROOT
+import sys
+
+INPUT_FILE = "/eos/user/r/reyu/full_chain_odd/fixparticleHypothesis/selection100Mev/n1500_pion/tracksummary_ckf.root"
+TREE_NAME = "tracksummary"
+
+PT_MIN = 0.8
+PT_MAX = 12
+
+#CUT = "nTracksTruthVtx > 0 && nTracksRecoVtx >= 2"
+#CUT = "vertex_primary == 1 &&  nTracksRecoVtx >= 2 && nTracksTruthVtx > 0"
+
+HIST_X_MIN, HIST_X_MAX = -0.1, 0.1
+HIST_BINS = 100
+
+FIT_RANGE_MIN, FIT_RANGE_MAX = -0.05,0.05
+
+f = ROOT.TFile.Open(INPUT_FILE)
+t = f.Get(TREE_NAME)
+
+c_slice = ROOT.TCanvas("c_slice", "Slice Inspection", 800, 600)
+c_slice.SetGrid() 
+
+ROOT.gStyle.SetOptFit(1111)
+ROOT.gStyle.SetOptStat(10) 
+
+hist_name = "h_slice_resT"
+hist_title = f"res_ePHI_fit Slice: {PT_MIN:.1f} < t_pT < {PT_MAX:.1f} GeV;mm;Entries / bin"
+h_slice = ROOT.TH1D(hist_name, hist_title, HIST_BINS, HIST_X_MIN, HIST_X_MAX)
+
+full_cut = f"t_pT > {PT_MIN} && t_pT < {PT_MAX} "
+print("-" * 60)
+print(f"{full_cut}")
+print("-" * 60)
+t.Draw(f"res_ePHI_fit >> {hist_name}", full_cut)
+
+entries = h_slice.GetEntries()
+print(f" {int(entries)} vertex")
+
+fit_func = ROOT.TF1("gaus", "gaus", FIT_RANGE_MIN, FIT_RANGE_MAX)
+fit_func.SetLineColor(ROOT.kRed) 
+fit_func.SetLineWidth(3)
+
+
+print(f"fit in  [{FIT_RANGE_MIN}, {FIT_RANGE_MAX}] ")
+
+fit_status = h_slice.Fit(fit_func, "R") 
+#fit_status = h_slice.Fit("gaus", "", "", FIT_RANGE_MIN, FIT_RANGE_MAX)
+h_slice.SetMarkerStyle(20) 
+h_slice.SetMarkerSize(0.8)
+h_slice.SetLineColor(ROOT.kBlack)
+
+h_slice.Draw("E")
+c_slice.Update()
+
+st = h_slice.FindObject("stats")
+if st:
+    st.SetX1NDC(0.65) 
+    st.SetX2NDC(0.90) 
+    st.SetY1NDC(0.60) 
+    st.SetY2NDC(0.90) 
+#output_filename = f"SliceFit_{PT_MIN}to{PT_MAX}GeV.png"
+#c_slice.SaveAs(output_filename)
+
+c_slice.Draw()
+
